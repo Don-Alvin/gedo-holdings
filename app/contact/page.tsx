2c55877f434f6f197e6bbd1e809c5882936a2f4f
Don Alvin <66693114+Don-Alvin@users.noreply.github.com>
2026-06-08 17:17:10 +0300

    location map

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,7 +46,7 @@ export default function ContactPage() {
             Grey Park Annex, Eastern Bypass
           </h2>
           <iframe
-            src="https://maps.app.goo.gl/u2UqaJCjTP4qqxsC6"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8282989455206!2d36.97020279999999!3d-1.2764163000000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f6bdc9cc3a817%3A0xd81ae7b36478834e!2sGrey%20Park%20Annex!5e0!3m2!1sen!2ske!4v1780928148211!5m2!1sen!2ske"
             width="100%"
             height="450"
             style={{ border: 0 }}
