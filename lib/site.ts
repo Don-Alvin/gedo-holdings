7d6dd0bd620624181fa163d82f9c6776b9045b1c
Don Alvin <66693114+Don-Alvin@users.noreply.github.com>
2026-06-08 17:12:49 +0300

    site url error

--- lib/site.ts
+++ lib/site.ts
@@ -1,3 +1,3 @@
-export const SITE_URL = process.env.SITE_URL || "https://gedoholdings.vercel.app";
+export const SITE_URL = process.env.SITE_URL || "https://gedoholdings.co.ke";
 export const SITE_NAME = "Gedo Holdings";
 export const SITE_TITLE = "Gedo Holdings Ltd: Building Dreams, Enhancing Lives.";
